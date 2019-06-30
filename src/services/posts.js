4cd332a1ee5e705ba66f0832d6de687bd781d004
Giovanne <giovanneafonso@gmail.com>
2019-06-30 17:41:37 -0400

    Seed db with hello world post

--- src/services/posts.js
+++ src/services/posts.js
@@ -10,5 +10,25 @@ broker.createService({
   version: 1,
   mixins: [DbService],
   adapter: dbadapter,
-  model: Post
+  model: Post,
+  methods: {
+    async seedDB() {
+      this.logger.info('Seed posts collection...')
+
+      await this.adapter.insert({
+        slug: 'hello-world',
+        title: 'Hello World',
+        excerpt: 'This post is only a demo to populate DB',
+        body: '<h2>It works!</h2>\n<p>Content of the post here</p>',
+        image: 'https://placehold.it/300x300?text=Hello'
+      })
+    }
+  },
+  afterConnected() {
+    return this.adapter.count().then(async count => {
+      if(count === 0) {
+        await this.seedDB()
+      }
+    })
+  }
 })
